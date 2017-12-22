a1df741172d15d2c38aa16c1145e91d6c8852670
YongJin Jung <30520764+greensori@users.noreply.github.com>
2017-12-22 17:45:56 +0900

    Update cnclt.cpp

--- cnclt.cpp
+++ cnclt.cpp
@@ -6,7 +6,6 @@
 cnclt::cnclt(int a)
 {
   _pinReader = 0;
-  _switchStatus = 0;
 }
 
 int cnclt::initialize(){
@@ -17,3 +16,13 @@ int cnclt::endSwitchStatus(int d, int pinno) {
   _pinReader = digitalRead(pinno);
   return pinResult;
 }
+
+void cnclt::endSwitchChecker(int DIR, int STEP, int endswitch) {
+  digitalWrite(DIR, HIGH);
+  while (digitalRead(endswitch) == 0) {
+    digitalWrite(STEP, LOW);
+    digitalWrite(STEP, HIGH);
+    delayMicroseconds(1000);
+  }
+}
+
