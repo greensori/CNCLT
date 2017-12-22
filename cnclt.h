219c051c2ded1c2705abdb67977628d3cd068939
YongJin Jung <30520764+greensori@users.noreply.github.com>
2017-12-22 10:05:28 +0900

    Create cnclt.h

---
+++ cnclt.h
@@ -0,0 +1,19 @@
+#ifndef cnclt_h
+#define cnclt_h
+
+#include "Arduino.h"
+
+class cnclt
+{
+  private:
+    int _test;
+    int _switchStatus;
+    int _pinReader;
+    int _count;
+  public:
+    cnclt(int a);
+    int initialize();
+    int endSwitchStatus(int d, int pinno);
+};
+
+#endif
