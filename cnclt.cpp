ab67f9a92e55f0f35e06360b14cdf42d94eec15a
YongJin Jung <30520764+greensori@users.noreply.github.com>
2017-12-27 11:55:13 +0900

    Update cnclt.cpp

--- cnclt.cpp
+++ cnclt.cpp
@@ -12,17 +12,35 @@ int cnclt::initialize(){
   return 1;
 }
 
+
+/*
+ * this module to checking end switch status
+ */
 int cnclt::endSwitchStatus(int d, int pinno) {
   _pinReader = digitalRead(pinno);
   return pinResult;
 }
 
+
+/*
+ * this is using for initiating machine
+ */
 void cnclt::endSwitchChecker(int DIR, int STEP, int endswitch) {
   digitalWrite(DIR, HIGH);
   while (digitalRead(endswitch) == 0) {
     digitalWrite(STEP, LOW);
     digitalWrite(STEP, HIGH);
     delayMicroseconds(1000);
+  }
+  while (digitalRead(endswitch) == 1) {
+    digitalWrite(STEP, LOW);
+    digitalWrite(STEP, HIGH);
+    delayMicroseconds(1500);
+  }
+  while (digitalRead(endswitch) == 0) {
+    digitalWrite(STEP, LOW);
+    digitalWrite(STEP, HIGH);
+    delayMicroseconds(1500);
   }
 }
 
