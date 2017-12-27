4c3222c33bf23b05e469d07bb65b90356aa22a2a
YongJin Jung <30520764+greensori@users.noreply.github.com>
2017-12-27 17:31:27 +0900

    Update cnclt.cpp

--- cnclt.cpp
+++ cnclt.cpp
@@ -3,11 +3,14 @@
 
 #define pinResult (d + _pinReader)
 
+#define returnPosition (stepcount - 1)
+
 cnclt::cnclt(int a)
 {
   _pinReader = 0;
 }
 
+ 
 int cnclt::initialize(){
   return 1;
 }
@@ -21,11 +24,21 @@ int cnclt::endSwitchStatus(int d, int pinno) {
   return pinResult;
 }
 
+// input pts.stepCount[n],
+int cnclt::stepWorkPosition(int pinno, int stepcount){
+  if (digitalRead(pinno) == 1) {
+    digitalWrite(STEP, LOW);
+    digitalWrite(STEP, HIGH);
+    return (stepcount - 1);
+  } else if (digitalRead(pinno) == 0) {
+    return 0;
+  }
+}
 
 /*
  * this is using for initiating machine
  */
-void cnclt::endSwitchChecker(int DIR, int STEP, int endswitch) {
+int cnclt::endSwitchChecker(int DIR, int STEP, int endswitch) {
   digitalWrite(DIR, HIGH);
   while (digitalRead(endswitch) == 0) {
     digitalWrite(STEP, LOW);
@@ -33,6 +46,7 @@ void cnclt::endSwitchChecker(int DIR, int STEP, int endswitch) {
     delayMicroseconds(1000);
   }
   while (digitalRead(endswitch) == 1) {
+    digitalWrite(DIR, LOW);
     digitalWrite(STEP, LOW);
     digitalWrite(STEP, HIGH);
     delayMicroseconds(1500);
@@ -42,5 +56,6 @@ void cnclt::endSwitchChecker(int DIR, int STEP, int endswitch) {
     digitalWrite(STEP, HIGH);
     delayMicroseconds(1500);
   }
+  return 0;
 }
 
