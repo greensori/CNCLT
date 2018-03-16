3bed38873281463c593479c6f3cdc6ebf2d1f372
YongJin Jung <30520764+greensori@users.noreply.github.com>
2018-03-16 17:56:56 +0900

    Update myDev.h

--- myDev.h
+++ myDev.h
@@ -3,7 +3,6 @@
 
 #include "Arduino.h"
 
-
 class myDev
 {
   private:
@@ -16,15 +15,14 @@ class myDev
     uint8_t xAxisEndPin;
     uint8_t rAxisStartPin;
     uint8_t rAxisEndPin;
-    uint8_t homePtPhase;
+    //uint8_t homePtPhase;
     uint16_t pts[2];
-    uint16_t stepPwm[2] = {900, 900};
+    //uint16_t stepPwm[2] = {900, 900};
     int i;
-    char inChar;
   public:
     myDev();
     ~myDev();
-    uint8_t asciiGet(char inChar);
+    uint8_t asciiGet(uint8_t inChar);
     void registerPinEna(uint8_t xAxisEna1, uint8_t xAxisEna2, uint8_t rAxisEna);
     void registerPinDir(uint8_t xAxisDir1, uint8_t xAxisDir2, uint8_t rAxisDir);
     void registerPinStep(uint8_t xAxis1, uint8_t xAxis2, uint8_t rAxis);
@@ -33,6 +31,8 @@ class myDev
     uint16_t xAxisMovement();
     uint16_t rAxisMovement();
     void setDirection(uint8_t targetStep, uint8_t setDir);
+    void xAxisHome();
+    void rAxisHome();
     void homeAll();
 };
 
