4fdcb502f83637f5f702337cdb8c9a0fdfe3deb6
YongJin Jung <30520764+greensori@users.noreply.github.com>
2018-03-20 14:33:48 +0900

    Update myAscii.cpp

--- myAscii.cpp
+++ myAscii.cpp
@@ -10,12 +10,21 @@ myAscii::~myAscii()
 {
 }
 
+uint16_t myAscii::asciiToPts(char inChar, uint16_t *ptA, uint16_t *ptB, uint16_t *ptC) {
+  //65 to 90
+  if (inChar <= 75) {
+    
+  } else if (inChar <= 85) {
+    
+  } else if (inChar <= 90) {
+    
+  }
+}
+
 uint16_t myAscii::asciiToPwm(char inChar, uint16_t *digitA, uint16_t *digitB, uint16_t *digitC) {
   if (inChar <= 106) {
-    Serial.println ("enter inchar A");
     *digitA = ((inChar - 97) * 10);
   } else if (inChar <= 116) {
-    Serial.println ("enter inchar B");
     *digitB = ((inChar - 107) * 100);
   } else if (inChar <= 122) {
     *digitC = ((inChar - 117) * 1000);
